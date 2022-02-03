1a408cae04e58bc89dff52dfd763450c4b580a66
daveh <dave.halladay@outlook.com>
2022-02-02 22:11:30 -0700

    update seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -8,5 +8,18 @@ const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  
-}
+  console.log('--Seeding Users--');
+  await seedUser();
+  console.log('--Seeding Category--');
+  await seedCategory();
+  console.log('--Seeding Solution--');
+  await seedSolution();
+  console.log('--Seeding Tags--');
+  await seedTag();
+  console.log('--Seeding SolveTag--');
+  await seedSolveTag();
+
+  process.exit(0);
+};
+
+seedAll();
