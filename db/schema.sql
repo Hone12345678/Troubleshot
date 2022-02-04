af439d0a1e37aba9b56cce641f03b6615b9480e2
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2022-02-03 21:20:53 -0700

    setting up index in models folder during group meeting

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 0f78e610920ed3713c56efcd1ca90724b36d4ac7
 -- DROP DATABASE
 DROP DATABASE IF EXISTS troubleshot_db;
 
 -- CREATE DATABASE
 CREATE DATABASE troubleshot_db;
-<<<<<<< HEAD
-=======
-
->>>>>>> 0f78e610920ed3713c56efcd1ca90724b36d4ac7
--- models/index.js
+++ models/index.js
@@ -5,11 +5,60 @@ const SolveTag = require('./SolveTag');
 const Tag = require('./Tag');
 const User = require('./User');
 
-//User has many Solutions 
 
-//SolveTags belong to many 
+// user has many categories
+User.hasMany(Category, {
+    foreignKey: 'user_id'
+})
+// category belongs to one user
+Category.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+// category has only one user
+Category.hasOne(User,{
+    foreignKey: 'user_id'
+})
 
-//Solution belongsToMany Categories 
+// category has many solution
+Category.hasMany(Solution,{
+    foreignKey: 'category_id'
+})
+
+// solution has one category
+Solution.hasOne(Category, {
+    foreignKey: 'category_id'
+})
+
+// solution belongs to one category
+Solution.belongsTo(Category,{
+    foreignKey: 'solution_id'
+})
+
+// user has many solutions
+User.hasMany(Solution,{
+    foreignKey: 'user_id'
+})
+
+// solution has many tags
+Solution.hasMany(Tag,{
+    foreignKey: 'solution_id'
+})
+
+// solution belongs to many tags
+Solution.hasMany(Tag,{
+    foreignKey: 'solution_id'
+})
+
+// tag has many solutions
+Tag.hasMany(Solution,{
+    foreignKey: 'tag_id'
+})
+
+// tag belongs to many solutions
+Tag.belongsTo(Solution,{
+    foreignKey: 'solution_id'
+})
+
 
 
 module.exports = {
