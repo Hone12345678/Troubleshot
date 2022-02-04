ecc8cd76a674a90e587ef42aa5dcf60ccb8ef564
lnguyen87 <luc_nguyen@outlook.com>
2022-02-04 13:12:11 -0700

    restyled page, added buttons for topics, added buttons for categories, made page mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
       animation: {
         'spin-slow': 'spin 2s linear infinite',
         'pulse-slow': 'pulse 2s ease-in-out infinite',
-      }
+      },
+      width: {
+        '45': "45%",
+      },
+      screens: {
+        'xs': '325px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [],
