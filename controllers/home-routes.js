fa1abeb98b7357c6394dbe5c5656f4002460c661
daveh <dave.halladay@outlook.com>
2022-02-04 20:34:40 -0700

    update home-routes.js to pass in category array and template to dyamically generate cat buttons

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,21 @@
+const { Category } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  Category.findAll({
+    attributes: [
+      'category_name'
+    ]
+  })
+    .then(catData => {
+      const categories = catData.map(category => category.get({ plain: true }));
+      res.render('homepage', { categories });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
