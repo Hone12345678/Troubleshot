42c93b7a281a887bef4f833d3527b902b4de7e32
Ross Westwater <ross.westwater@gmail.com>
2022-02-04 18:17:32 -0700

    commented out extraneous relationships

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -1,10 +1,67 @@
-const router = require('express').Router();
-const {Category, Solution, SolveTag, Tag, User} = require('../../models')
+const router = require("express").Router();
+const { Category, Solution, SolveTag, Tag, User } = require("../../models");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Category.findAll()
-    .then(dbCategoryData => res.json(dbCategoryData))
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Category.create(req.body, {
+    category_name: req.body.category_name,
+    user_id: req.body.user_id,
+  })
+    .then((dbCategoryData) => {
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+    category_name: req.body.category_name,
+  })
+    .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+
+router.delete("/:id", (req, res) => {
+  Category.destroy({
+    when: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => {
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
