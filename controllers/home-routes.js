429281eee3e9d7177018fcd42c738fcd96142a76
daveh <dave.halladay@outlook.com>
2022-02-05 11:53:22 -0700

    prepare to push

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   //perform multiple queries in one get request using async/await
-  try{
+  try {
     const categoryQuery = await Category.findAll({
       attributes: [
         'category_name'
@@ -17,27 +17,27 @@ router.get('/', async (req, res) => {
         'id', 'name', 'solution', 'priority', 'category_id', 'user_id'
       ]
     });
-    
+
     //use .map() method on query arrays so that we only get values from table
     const categories = categoryQuery.map(array => array.get({ plain: true }));
     const sol = solutionQuery.map(array => array.get({ plain: true }));
-    
+
     //send the new arrays to the homepage to be displayed on the home page
     res.render('homepage', {
       categories,
       sol
     })
   }
-    catch(err) {
-      console.log(err);
-      res.status(500).json(err);
-    };
+  catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
 });
 
 //get route for highest-priority button
 router.get('/highest-priority', async (req, res) => {
   //perform multiple queries in one get request using async/await
-  try{
+  try {
     const categoryQuery = await Category.findAll({
       attributes: [
         'category_name'
@@ -53,21 +53,21 @@ router.get('/highest-priority', async (req, res) => {
         ['priority', 'DESC']
       ]
     });
-    
+
     //use .map() method on query arrays so that we only get values from table
     const categories = categoryQuery.map(array => array.get({ plain: true }));
     const sol = solutionQuery.map(array => array.get({ plain: true }));
-    
+
     //send the new arrays to the homepage to be displayed on the home page
     res.render('homepage', {
       categories,
       sol
     })
   }
-    catch(err) {
-      console.log(err);
-      res.status(500).json(err);
-    };
+  catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  };
 });
 
 module.exports = router;
