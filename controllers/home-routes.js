cb55bb1e3fc63f7166e0c48ee8f0f16cf526788d
daveh <dave.halladay@outlook.com>
2022-02-04 22:07:31 -0700

    remove old code comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,67 +2,6 @@ const { Category, Solution } = require('../models');
 
 const router = require('express').Router();
 
-// router.get('/', (req, res) => {
-//   Category.findAll({
-//     attributes: [
-//       'category_name'
-//     ]
-//   })
-//     .then(catData => {
-//       const categories = catData.map(category => category.get({ plain: true }));
-//       res.render('homepage', { categories });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-//   Solution.findAll({
-//     where: {
-//       id: 2
-//     },
-//     attributes: [
-//       'id', 'name', 'solution', 'priority', 'category_id', 'user_id'
-//     ]
-//   })
-//     .then(solutionData => {
-//       const solutions = solutionData.map(sol => sol.get({ plain: true }));
-//       //remove/replace this when we figure out how many solutions to display on homepage
-//       const oneSolution = solutions[0];
-
-//       res.render('homepage', { oneSolution });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-// router.get('/', (req, res) => {
-//   Category.findAll({
-//     attributes: [
-//       'category_name'
-//     ]
-//   })
-//   Solution.findAll({
-//     where: {
-//       id: 2
-//     },
-//     attributes: [
-//       'id', 'name', 'solution', 'priority', 'category_id', 'user_id'
-//     ]
-//   })
-//     .then(queryData => {
-//       // const query = queryData.map(array => array.get({ plain: true }));
-//       console.log(queryData);
-//       // const categories = catData.map(category => category.get({ plain: true }));
-//       // res.render('homepage', { categories });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
 router.get('/', async (req, res) => {
   try{
     const categoryQuery = await Category.findAll({
