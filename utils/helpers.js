3fa9abcd68fda2e85cc5f65de5bb2d9740b4f56a
daveh <dave.halladay@outlook.com>
2022-02-04 22:39:46 -0700

    added ability to search for highest priority items on homepage

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,5 @@
 module.exports = {
+  //capitalize the first letter of the word/sentence (whatever is in the array)
   capital_category: cat => {
     return cat.charAt(0).toUpperCase() + cat.slice(1);
   }
