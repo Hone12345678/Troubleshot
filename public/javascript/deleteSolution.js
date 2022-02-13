4edc9a839d3f8251b691d74b4214b99bdbb24a17
daveh <dave.halladay@outlook.com>
2022-02-13 15:04:11 -0700

    add delete button modal functionality

--- public/javascript/deleteSolution.js
+++ public/javascript/deleteSolution.js
@@ -1,17 +1,42 @@
-// allows user to target a specific solution to delete
-async function deleteButtonHandler(event) {
-    event.preventDefault();
+const deleteModal = document.getElementById('deleteConfirm');
+const delBtn = document.querySelector('#deleteBtn');
+let id = '';
 
-    const id = event.target.getAttribute('data-delId');   
-    const response = await fetch(`/api/solutions/${id}`, {
-        method: 'DELETE'
-    });
-    
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-   
-document.querySelector('#deleteBtn').addEventListener('click', deleteButtonHandler);
+// allows user to target a specific solution to delete
+async function deleteButtonHandler() {
+  const response = await fetch(`/api/solutions/${id}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+//toggle modal visibility
+function toggleModal() {
+  deleteModal.classList.toggle('hidden')
+}
+
+// confirm-delete
+document.querySelector('#confirm-delete').addEventListener('click', deleteButtonHandler);
+
+// cancel-delete
+document.querySelector('#cancel-delete').addEventListener('click', toggleModal);
+
+//toggle modal on clicking delete button
+delBtn.addEventListener('click', (event)=> {
+  event.preventDefault();
+  id = event.target.getAttribute('data-delId');
+  console.log(id)
+  toggleModal();
+  return id;
+});
+
+window.onclick = function(event) {
+  if (event.target === deleteModal) {
+    deleteModal.classList.toggle('hidden')
+  }
+}
