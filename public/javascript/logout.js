b6374a59e6c4880bfb4ea693c28ed47b2af905c9
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2022-02-12 19:10:43 -0700

    adding comments to the public folder contents

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,3 +1,4 @@
+//allows user to logout
 async function logout() {
   const response = await fetch('/api/users/logout', {
     method: 'post',
--- public/javascript/quickSearch.js
+++ public/javascript/quickSearch.js
@@ -3,9 +3,12 @@ async function searchButtonHandler(event) {
     event.preventDefault();
 
   const searchVal = document.querySelector('#search').value.trim();
-    
+
+
+//if the user has entered text into the search bar they are routed to the specified category 
     if (searchVal) {
       document.location.replace(`/search-category/${searchVal}`)
+//when an empty search is submitted the user is just routed back to the main page
     } else {
       document.location.replace('/')
     }
