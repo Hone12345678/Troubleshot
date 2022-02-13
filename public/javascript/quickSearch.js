cd7edcbbb719452b63000923d0e9c0453ec8196f
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2022-02-12 17:49:39 -0700

    fixing search bar empty search bug

--- public/javascript/quickSearch.js
+++ public/javascript/quickSearch.js
@@ -1,12 +1,15 @@
-
 //generalsearch
 async function searchButtonHandler(event) {
     event.preventDefault();
 
   const searchVal = document.querySelector('#search').value.trim();
     
+    if (searchVal) {
       document.location.replace(`/search-category/${searchVal}`)
+    } else {
+      document.location.replace('/')
     }
+  }
   
   document.querySelector('#submitBtn').addEventListener('click', searchButtonHandler);
 
